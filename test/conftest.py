e989d4d7851a488ed74685e44aa8da158d488a75
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-07-25 15:23:56 +0200

    Test Fixtures and Data Providers: * Update conftest - add method create_depth

--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,19 @@
 import pytest
 
 
+def create_depth(depth: int) -> str:
+    """ Creates a string of space chars equivalent to the given depth.
+
+**Parameters:**
+ - depth (int): The amount of depth in the Tree Node Structure.
+
+**Returns:**
+ str - The String of Space Char, of the required length.
+	"""
+    # Two space characters per unit of depth
+    return '  ' * depth
+
+
 def raise_exception(name: str):
     """ Raise an Exception in a mock method.
  - Argument is lowercased before matching.
