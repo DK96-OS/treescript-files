667a628fe61b4e915df16ca9bb97e3d9a02ed725
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-07-25 15:25:01 +0200

    Test: * Update __init__ - remove method create_depth (belongs in conftest)

--- test/__init__.py
+++ test/__init__.py
@@ -1,15 +1,2 @@
-"""Initialize Test Module.
+""" Initialize Test Module.
 """
-
-
-def create_depth(depth: int) -> str:
-    """Creates a string of space chars equivalent to the given depth.
-
-	Parameters:
-	- depth (int): The amount of depth in the Tree Node Structure.
-
-	Returns:
-	str: The String of Space Char, of the required length.
-	"""
-	# Two space characters per unit of depth
-    return '  ' * depth
