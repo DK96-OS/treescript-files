bfea48cad46ccf3f3f228f5b33dd330506609fb6
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-07-25 18:29:52 +0200

    Project Root: * Update setup - version 0.2.3, set python_requires minimum 3.9

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-"""Setup Package Configuration
+""" Setup Package Configuration
 """
 from setuptools import setup, find_packages
 
 
 setup(
     name="treescript-files",
-    version="0.2.2",
+    version="0.2.3",
     description="Obtains the relative path of all files in the TreeScript.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -23,13 +23,14 @@ setup(
             'treescript_files=treescript_files.__main__:main',
         ],
     },
+    python_requires='>=3.9',
+    keywords=['treescript'],
     classifiers=[
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
