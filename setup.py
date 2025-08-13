7bfd73993ae928135aef86387e51a8d8ae49e705
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2025-08-13 15:50:46 +0200

    Release 0.2.4 (#11)
    Dependabot Update [actions/checkout](https://github.com/actions/checkout) from 4 to 5:
    * [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    * [Commits](https://github.com/actions/checkout/compare/v4...v5)
    * [Release notes](https://github.com/actions/checkout/releases)
    
    GitHub Workflows:
    * Update ci_run - upgrade pytest 8.4.1, upgrade pytest-cov 6.2.1, increase max-parallel runners to 6
    * Update publish - upgrade dk96-os/gh-action-sigstore-python v3.2.8
    
    Project Root:
    * Update setup.py - version 0.2.4, increase min required python version 3.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="treescript-files",
-    version="0.2.3",
+    version="0.2.4",
     description="Obtains the relative path of all files in the TreeScript.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -23,7 +23,7 @@ setup(
             'treescript_files=treescript_files.__main__:main',
         ],
     },
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     keywords=['treescript'],
     classifiers=[
         'Natural Language :: English',
@@ -31,7 +31,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
