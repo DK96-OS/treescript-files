883859cc02e25d77741c8ec756dfe112a57b5efa
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2025-09-01 19:39:48 +0200

    Project Root:
    * Update setup.py - version 0.2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="treescript-files",
-    version="0.2.4",
+    version="0.2.5",
     description="Obtains the relative path of all files in the TreeScript.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
