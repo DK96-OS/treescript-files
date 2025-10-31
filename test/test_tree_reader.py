b886e0e39d248d90a96fc331743357fce1216c22
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-10-31 10:02:24 -0400

    Test: * Update test_tree_reader - parametrize test cases with options for ParentPath Prefixes, add test cases for generate_treescript_files generator method

--- test/test_tree_reader.py
+++ test/test_tree_reader.py
@@ -1,10 +1,15 @@
 """ Testing Tree Reader and Process Input Data Method
 """
+from re import escape
+
+import pytest
+
+from treescript_files import line_reader, generate_treescript_files
 from treescript_files.input_data import InputData
 from treescript_files.tree_reader import process_input_data
 
 
-def test_process_input_data_single_file():
+def test_process_input_data_single_file_no_parent_path_returns_valid_path():
     input_data = InputData(
         tree_input='src/\n  file.py',
         parent_path=None
@@ -14,20 +19,36 @@ def test_process_input_data_single_file():
     assert result[0] in ['src/file.py', 'src\\file.py']
 
 
-def test_process_input_data_parent_path():
+def test_process_input_data_invalid_indentation_raises_exit():
+    input_data = InputData(
+        tree_input='src/\n file.py',
+        parent_path=None
+    )
+    with pytest.raises(SystemExit, match=escape(line_reader._INVALID_DEPTH_ERROR_MSG)):
+        result = list(process_input_data(input_data))
+
+
+@pytest.mark.parametrize(
+    'parent_path', [
+        'module/',
+        'module',
+        'module\\',
+    ]
+)
+def test_process_input_data_parametrized_parent_path_returns_valid_path(parent_path: str):
     input_data = InputData(
         tree_input='src/\n  file.py',
-        parent_path='module/'
+        parent_path=parent_path,
     )
     result = list(process_input_data(input_data))
     assert len(result) == 1
     assert result[0] in ['module/src/file.py', 'module\\src\\file.py']
 
 
-def test_process_input_data_two_files():
+def test_process_input_data_two_files_returns_valid_paths():
     input_data = InputData(
         tree_input='src/\n  file.py\n  file2.py',
-        parent_path=None
+        parent_path=None,
     )
     result = list(process_input_data(input_data))
     assert len(result) == 2
@@ -35,12 +56,46 @@ def test_process_input_data_two_files():
     assert result[1] in ['src/file2.py', 'src\\file2.py']
 
 
-def test_process_input_data_two_files_parent_path():
+@pytest.mark.parametrize(
+    'parent_path', [
+        'module/',
+        'module',
+        'module\\',
+    ]
+)
+def test_process_input_data_two_files_parent_path_returns_valid_paths(parent_path: str):
     input_data = InputData(
         tree_input='src/\n  file.py\n  file2.py',
-        parent_path='module/'
+        parent_path=parent_path,
     )
     result = list(process_input_data(input_data))
     assert len(result) == 2
     assert result[0] in ['module/src/file.py', 'module\\src\\file.py']
     assert result[1] in ['module/src/file2.py', 'module\\src\\file2.py']
+
+
+@pytest.mark.parametrize(
+    'parent_path', [
+        'module/',
+        'module',
+        'module\\',
+    ]
+)
+def test_generate_treescript_files_parametrized_parent_path_returns_valid_paths(parent_path: str):
+    generator = generate_treescript_files(
+        treescript_file='src/\n  file.py\n  file2.py DataLabel # Comment',
+        parent_path=parent_path,
+    )
+    result = list(generator)
+    assert len(result) == 2
+    assert result[0] in ['module/src/file.py', 'module\\src\\file.py']
+    assert result[1] in ['module/src/file2.py', 'module\\src\\file2.py']
+
+
+def test_generate_treescript_files_invalid_indentation_raises_exit():
+    generator = generate_treescript_files(
+        treescript_file='src/\n file.py',
+        parent_path=None
+    )
+    with pytest.raises(SystemExit, match=escape(line_reader._INVALID_DEPTH_ERROR_MSG)):
+        list(generator)
